77ef1e9adb6a03431db910770acc2c68ea1da718
Igli2 <sg.gerdon@gmx.de>
2023-09-30 18:03:55 +0200

    added tilemap and tileset for testing

---
+++ assets/tileset.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="tileset" tilewidth="32" tileheight="32" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="tilemap/dirt.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="tilemap/grass_edge.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="tilemap/grass_inner_corner.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="tilemap/grass_outer_corner.png"/>
+ </tile>
+</tileset>
