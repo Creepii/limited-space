6892522d020152cec442d79b556b0c378efcbe2b
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2023-09-30 21:12:47 +0200

    merged changes

--- src/level.rs
+++ src/level.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 use crate::{
     atlas::TilemapAtlas,
     tilemap::{TileSet, Tilemap, TilemapAtlasResolver, Tiles},
-    GameStates,
+    GameStates, MainCamera,
 };
 
 pub struct LevelPlugin;
@@ -12,21 +12,54 @@ impl Plugin for LevelPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(GameStates::Level), setup_level);
         app.insert_resource(CurrentCharacter {
-            current: Character::Turtle,
+            current: Character::Rabbit,
         });
-        app.add_systems(Update, player_movement);
+        app.insert_resource(CurrentCameraMode {
+            current: CameraMode::AllCharacters,
+        });
+        app.add_systems(
+            Update,
+            switch_camera_mode.run_if(in_state(GameStates::Level)),
+        );
+        app.add_systems(Update, player_movement.run_if(in_state(GameStates::Level)));
+        app.add_systems(Update, camera_movement.run_if(in_state(GameStates::Level)));
         app.add_systems(OnExit(GameStates::Level), cleanup_level);
+    }
+}
+
+fn switch_camera_mode(keys: Res<Input<KeyCode>>, mut camera_mode: ResMut<CurrentCameraMode>) {
+    if keys.just_pressed(KeyCode::F) {
+        camera_mode.toggle();
     }
 }
 
 #[derive(Component, PartialEq, Eq)]
 enum Character {
     Turtle,
+    Rabbit,
 }
 
 #[derive(Resource)]
 struct CurrentCharacter {
     current: Character,
+}
+
+enum CameraMode {
+    AllCharacters,
+    CurrentCharacter,
+}
+impl CurrentCameraMode {
+    fn toggle(&mut self) {
+        match self.current {
+            CameraMode::AllCharacters => self.current = CameraMode::CurrentCharacter,
+            CameraMode::CurrentCharacter => self.current = CameraMode::AllCharacters,
+        }
+    }
+}
+
+#[derive(Resource)]
+struct CurrentCameraMode {
+    current: CameraMode,
 }
 
 fn setup_level(
@@ -44,12 +77,22 @@ fn setup_level(
         },
         Character::Turtle,
     ));
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("characters/rabbit.png"),
+            transform: Transform::from_xyz(0.0, 64.0, 0.0),
+            ..default()
+        },
+        Character::Rabbit,
+    ));
+
     let tiles_asset: Handle<Tiles> = asset_server.load("levels/level1/tilemap_ground.csv");
     let tile_set_asset: Handle<TileSet> = asset_server.load("levels/level1/tilset.json");
     let tilemap = Tilemap::new(
         tile_set_atlas.get(&tile_set_asset).unwrap(),
         tiles_atlas.get(&tiles_asset).unwrap(),
-    ).unwrap();
+    )
+    .unwrap();
     let tilemap_resolver =
         TilemapAtlasResolver::new(&tilemap, asset_server, tilemap_atlas, atlasses);
     for x in 0..tilemap.width() {
@@ -68,7 +111,7 @@ fn setup_level(
     }
 }
 
-const PLAYER_SPEED: f32 = 64.0;
+const PLAYER_SPEED: f32 = 256.0;
 
 fn player_movement(
     time: Res<Time>,
@@ -102,3 +145,63 @@ fn player_movement(
 }
 
 fn cleanup_level() {}
+
+const CAMERA_SCALE: f32 = 0.4;
+const CAMERA_PADDING: f32 = 128.0;
+
+fn camera_movement(
+    character: Res<CurrentCharacter>,
+    camera_mode: Res<CurrentCameraMode>,
+    mut param_set: ParamSet<(
+        Query<&mut Transform, With<MainCamera>>,
+        Query<&Window>,
+        Query<(&Character, &Transform)>,
+    )>,
+) {
+    match camera_mode.current {
+        CameraMode::AllCharacters => {
+            let character_transforms: Vec<Transform> =
+                param_set.p2().iter().map(|c| c.1.clone()).collect();
+            let ((min_x, max_x), (min_y, max_y)) = character_transforms
+                .iter()
+                .map(|t| (t.translation.x, t.translation.y))
+                .fold(
+                    ((f32::MAX, f32::MIN), (f32::MAX, f32::MIN)),
+                    |(px, py), (x, y)| ((x.min(px.0), x.max(px.1)), (y.min(py.0), y.max(py.1))),
+                );
+            let size_x = (min_x - max_x).abs() + CAMERA_PADDING;
+            let size_y = (min_y - max_y).abs() + CAMERA_PADDING;
+            let (window_width, window_height) = {
+                fn get_size(window: &Window) -> (f32, f32) {
+                    (window.width(), window.height())
+                }
+                get_size(param_set.p1().single())
+            };
+            let scale = (size_x / window_width).max(size_y / window_height);
+            let scale = scale.max(CAMERA_SCALE);
+            let center = Vec2::new((max_x + min_x) / 2.0, (max_y + min_y) / 2.0);
+            for mut camera_transform in param_set.p0().iter_mut() {
+                camera_transform.translation.x = center.x;
+                camera_transform.translation.y = center.y;
+                camera_transform.scale.x = scale;
+                camera_transform.scale.y = scale;
+            }
+        }
+        CameraMode::CurrentCharacter => {
+            let character_transform = param_set
+                .p2()
+                .iter()
+                .filter(|c| c.0 == &character.current)
+                .map(|c| c.1)
+                .next()
+                .unwrap()
+                .clone();
+            for mut camera_transform in param_set.p0().iter_mut() {
+                camera_transform.translation.x = character_transform.translation.x;
+                camera_transform.translation.y = character_transform.translation.y;
+                camera_transform.scale.x = CAMERA_SCALE;
+                camera_transform.scale.y = CAMERA_SCALE;
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -24,15 +24,19 @@ fn main() {
         .add_state::<GameStates>()
         .insert_resource(ClearColor(Color::BLACK))
         .add_systems(Startup, setup_base)
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                resizable: false,
-                resolution: WindowResolution::new(1024.0, 640.0),
-                title: String::from("Limited Space"),
-                ..default()
-            }),
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        resizable: false,
+                        resolution: WindowResolution::new(1024.0, 640.0),
+                        title: String::from("Limited Space"),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
         .add_plugins(TilemapAtlasPlugin)
         .add_plugins(LevelPlugin)
         .add_plugins(MenuPlugin)
@@ -43,6 +47,15 @@ fn main() {
         .run();
 }
 
+#[derive(Component)]
+pub struct MainCamera;
+
 fn setup_base(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        Camera2dBundle {
+            transform: Transform::default().with_scale(Vec3::new(0.25, 0.25, 0.25)),
+            ..default()
+        },
+        MainCamera,
+    ));
 }
