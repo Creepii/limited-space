876535c3de4226bee636df737a9a8e1a90b6fec3
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2023-09-30 14:46:56 +0200

    Implemented simple game menu

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowResolution};
+use menu::MenuPlugin;
+
+mod menu;
+
+#[derive(States, Debug, Default, Hash, Eq, PartialEq, Clone)]
+enum GameStates {
+    #[default]
+    Menu,
+    Level,
+}
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_state::<GameStates>()
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_systems(Startup, setup_base)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resizable: false,
+                resolution: WindowResolution::new(1024.0, 640.0),
+                title: String::from("Limited Space"),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins(MenuPlugin)
+        .run();
+}
+
+fn setup_base(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
---
+++ src/menu.rs
@@ -0,0 +1,152 @@
+use bevy::{
+    app::AppExit,
+    prelude::*,
+    text::{Text, TextStyle},
+    ui::{AlignItems, BackgroundColor, FlexDirection, JustifyContent, Style, UiRect, Val},
+    utils::default,
+};
+
+use crate::GameStates;
+
+pub struct MenuPlugin;
+
+impl Plugin for MenuPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(GameStates::Menu), setup_menu);
+        app.add_systems(Update, button_interactions);
+        app.add_systems(OnExit(GameStates::Menu), cleanup_menu);
+    }
+}
+
+#[derive(Component, Debug)]
+enum ButtonKinds {
+    Start,
+    Quit,
+}
+
+impl ButtonKinds {
+    fn get_color(&self, interaction: &Interaction) -> Color {
+        match self {
+            ButtonKinds::Start => Color::Hsla {
+                hue: 103.0,
+                saturation: 0.7,
+                lightness: match interaction {
+                    Interaction::Pressed => 0.6,
+                    Interaction::Hovered => 0.5,
+                    Interaction::None => 0.4,
+                },
+                alpha: 1.0,
+            },
+            ButtonKinds::Quit => Color::Hsla {
+                hue: 5.0,
+                saturation: 0.8,
+                lightness: match interaction {
+                    Interaction::Pressed => 0.7,
+                    Interaction::Hovered => 0.6,
+                    Interaction::None => 0.5,
+                },
+                alpha: 1.0,
+            },
+        }
+    }
+}
+
+fn button_interactions(
+    mut query: Query<(&Interaction, &mut BackgroundColor, &ButtonKinds), Changed<Interaction>>,
+    mut exit: EventWriter<AppExit>,
+    mut state: ResMut<NextState<GameStates>>,
+) {
+    for (interaction, mut bg_color, button) in &mut query {
+        *bg_color = BackgroundColor(button.get_color(interaction));
+        match *interaction {
+            Interaction::Pressed => match button {
+                ButtonKinds::Start => {
+                    state.set(GameStates::Level);
+                }
+                ButtonKinds::Quit => {
+                    exit.send(AppExit);
+                }
+            },
+            _ => (),
+        }
+    }
+}
+
+#[derive(Component)]
+struct MenuParent;
+
+fn setup_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("NotoSans-Regular.ttf");
+    let title_style = TextStyle {
+        font: font.clone(),
+        font_size: 100.0,
+        color: Color::WHITE,
+    };
+    let button_text_style = TextStyle {
+        font: font.clone(),
+        font_size: 50.0,
+        color: Color::WHITE,
+    };
+
+    info!("Setting up game menu");
+    commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    width: bevy::ui::Val::Percent(100.0),
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::Center,
+                    flex_direction: FlexDirection::Column,
+                    row_gap: Val::Px(10.0),
+                    ..default()
+                },
+                ..default()
+            },
+            MenuParent,
+        ))
+        .with_children(|p| {
+            p.spawn(TextBundle {
+                text: Text::from_section("Limited Space", title_style.clone()),
+                ..default()
+            });
+            p.spawn((
+                ButtonBundle {
+                    style: Style {
+                        border: UiRect::axes(Val::Px(5.0), Val::Px(5.0)),
+                        padding: UiRect::all(Val::Px(10.0)),
+                        width: Val::Percent(30.0),
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    ..default()
+                },
+                ButtonKinds::Start,
+            ))
+            .with_children(|p| {
+                p.spawn(TextBundle::from_section("Start", button_text_style.clone()));
+            });
+            p.spawn((
+                ButtonBundle {
+                    style: Style {
+                        border: UiRect::axes(Val::Px(5.0), Val::Px(5.0)),
+                        padding: UiRect::all(Val::Px(10.0)),
+                        width: Val::Percent(30.0),
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    ..default()
+                },
+                ButtonKinds::Quit,
+            ))
+            .with_children(|p| {
+                p.spawn(TextBundle::from_section("Quit", button_text_style.clone()));
+            });
+        });
+}
+
+fn cleanup_menu(mut commands: Commands, query: Query<Entity, With<MenuParent>>) {
+    info!("Destroying game menu");
+    for entity in &query {
+        commands.entity(entity).despawn();
+    }
+}
